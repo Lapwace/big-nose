941facef6211f5c42268ba23f5e3534c248d615c
Lapwace <lopo74000@gmail.com>
2021-04-13 05:57:12 +0200

    Panel Party without design

--- modules/function.mjs
+++ modules/function.mjs
@@ -18,13 +18,26 @@ function refresh_number_member(add_name, room_charged){
         member_list = document.querySelector("#party_list")
         if(member_list.innerHTML == undefined){
             let party_text = `<p>${add_name}`
-            member_list.innerHTML = party_text
+            document.querySelector("#party_list").innerHTML = party_text
         }else{
-            let party_text = member_list.innerHTML+`<p>${add_name}`
-            member_list.innerHTML = party_text
+            let party_text = document.querySelector("#party_list").innerHTML+`<p>${add_name}`
+            document.querySelector("#party_list").innerHTML = party_text
             socket.emit("refresh_room", currentRoom, party_text)
         }
     }
+}
+
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
 }
 
 function indexContent(){
@@ -43,7 +56,7 @@ function indexContent(){
 function mainContent(){
     document.querySelector("#content").innerHTML = `<div id="party_box"><div id="join_box"><button id="join_button">Join Party</button><input id="join_input" type="password" placeholder="PartyID"></input></div><button id="copy_button">Copy PartyID</button><button id="party_number"></button><div id="party_list"></div></div>`
     document.querySelector("#copy_button").addEventListener("click", (e) =>{
-        navigator.clipboard.writeText(`${userName+":"+userID}`)
+        copyToClipboard(userName+":"+userID)
     })
     document.querySelector("#join_button").addEventListener("click", (e) =>{
         let join_text = document.querySelector("#join_input")
